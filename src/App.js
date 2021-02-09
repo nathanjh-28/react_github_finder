f91bc5d261709803ae80f6027dde2379581fc247
nathanjh-28 <nathan@nathanjharris.com>
2021-02-09 16:46:13 -0700

    navbar component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>hello from react</h1>
+        <Navbar />
+        
       </div>
     );
   }
